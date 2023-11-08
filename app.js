7e952b664a1a5f977b8909bfba9ce92f838c699b
Sihle <msihlesndlovu97@gmail.com>
2023-11-08 21:17:04 +0200

    renderGame(), created array cardValues to store cards drawn

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-let firstCard = 10;
-let secondCard = 2;
+let firstCard = 3;
+let secondCard = 8;
 let newCard = 9;
+let cardValues = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
-//let the computer choose firstCard and secondCard
-function computerPicksCards() {
+//let the computer randomly choose firstCard and secondCard
+/* function computerPicksCards() {
   let pickRandomNumber = Number(1 + Math.round(Math.random() * 10));
   firstCard = pickRandomNumber;
   console.log(firstCard);
   pickRandomNumber = Number(1 + Math.round(Math.random() * 10));
   secondCard = pickRandomNumber;
   console.log(secondCard);
-} //nested scope of firstcard and secondcard prevents displaySum.textContent in startGame() from executing as expected => NaN as opposed to a number...maybe if i nest startGame() into computerPicksCards() then it might work???
-
-computerPicksCards();
+} */
 
 let messageAtStart = document.getElementById("message-at-start");
 let displaySum = document.querySelector("#display-sum");
 let displayCards = document.getElementById("display-cards");
 
-function startGame() {
+function renderGame() {
   displaySum.textContent = `Sum: ${sum}`;
-  displayCards.textContent = `Cards: ${firstCard} and ${secondCard}`;
+  displayCards.textContent = `Cards: ${cardValues[0]} and ${cardValues[1]}`;
   if (sum <= 20) {
-    message = "do you want to draw a new card?";
+    message = "you don't have blackjack, do you want to draw a new card?";
   } else if (sum === 21) {
     message = "you have black jack...well done";
     hasBlackJack = true;
@@ -36,6 +35,10 @@ function startGame() {
     isAlive = false;
   }
   messageAtStart.textContent = message;
+}
+
+function startGame() {
+  renderGame();
 }
 
 function issueNewCard() {
@@ -73,11 +76,7 @@ function issuesNewCard() {
   newCard = pickRandomNumber; */
   message = `your new card is: ${newCard}`;
   displayForNewCard.textContent = message;
+  console.log(message);
   console.log(sum);
   //beginning if statements and numeric operations
-  console.log(selectDiv);
-  console.log(typeof sum);
-  console.log(pickRandomNumber);
-  console.log(displayForNewCard);
-  console.log(typeof displayForNewCard);
 }
